082012dbe8c635b53bc18b90e952ec4d4d0c75e9
Critteros <szymonkoziol3@gmail.com>
2021-02-02 19:51:35 +0100

    Added some docstrings

--- app/configHandler.py
+++ app/configHandler.py
@@ -4,10 +4,17 @@ from pathlib import Path
 
 
 class Config:
+    """
+    This class is a template for Configurations whose atributes will be added directly the instance of this class. Property [properties] contain all attributes that config should have.
+    """
     properties: List[str] = [
         'log_to_file',
         'log_to_console',
         'console_logger_type',
+        'log_library',
+        'console_log_level',
+        'file_log_level',
+        'library_log_level',
         'discord_token_file'
     ]
 
