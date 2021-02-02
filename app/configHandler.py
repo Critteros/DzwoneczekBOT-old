d50e572f579def1a5931220b70eea48e0a4ceaff
Critteros <szymonkoziol3@gmail.com>
2021-02-02 19:19:34 +0100

    Added partial config system

---
+++ app/configHandler.py
@@ -0,0 +1,67 @@
+import json
+from typing import List
+from pathlib import Path
+
+
+class Config:
+    properties: List[str] = [
+        'log_to_file',
+        'log_to_console',
+        'console_logger_type',
+        'discord_token_file'
+    ]
+
+
+def loadConfig(file: Path) -> Config:
+    """
+    Loads config pointed by the file variable
+
+    Args:
+        file (Path): An Path object to the desired config file
+
+    Returns:
+        Config: Instance of Config class containing loaded config
+    """
+    # Empty file path exception
+    if(not file.exists()):
+        raise(FileNotFoundError)
+
+    config_return: Config = Config()
+
+    # Load JSON file to a string
+    raw_json: str = loadJSONtext(file)
+
+    # Try to Parse JSON file
+    try:
+        json_object: dict = json.loads(raw_json)
+    except json.JSONDecodeError:
+        raise RuntimeError(f'Invalid JSON: {file}')
+
+    # Read and save all properties that intrest us
+    for value in Config.properties:
+        try:
+            setattr(config_return, value, json_object[value])
+        except KeyError:
+            pass
+
+    return config_return
+
+
+def loadJSONtext(file: Path) -> str:
+    """
+    Loads JSON file to a string 
+
+    Args:
+        file (Path): Path object pointing to a JSON file
+
+    Returns:
+        str: A string representation of a file
+    """
+    # Checks if file exist
+    if(not file.exists()):
+        raise(FileNotFoundError)
+
+    with open(file, 'rt') as f:
+        string_json: str = f.read()
+
+    return string_json
--- startup.py
+++ startup.py
@@ -1,4 +1,27 @@
+from pathlib import Path
+
+# App includes
+from app.configHandler import loadConfig
+
+
+def main() -> None:
+    print("Hello World")
+
+    # Loding config paths
+    default_config_path: Path = Path('./app/default_config.json')
+    config_path: Path = Path('./config.json')
+
+    if(not default_config_path.exists()):
+        print("Default Config File not found!")
+        raise(FileNotFoundError)
+    if(not config_path.exists()):
+        print("Config File not found!")
+        raise(FileNotFoundError)
+
+    print(loadConfig(default_config_path).__dict__)
+
+
 if __name__ != '__main__':
     print("Wrong file was run!\nRun file 'startup.py' instead")
-
-print("Hello World")
+else:
+    main()
