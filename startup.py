998e0d5c5e41ca086fa234713147b7a0b4fc3ada
Critteros <szymonkoziol3@gmail.com>
2021-02-03 19:33:33 +0100

    Finished refractoring LoggerCore.py

--- startup.py
+++ startup.py
@@ -29,6 +29,7 @@ def main() -> None:
     Quick summary of how application starts:
     1) Configs are loaded from JSONs and parsed
     2) Setup Loggers    
+    3)Load all needed tokens
     """
     print("Starting up!")
 
@@ -43,16 +44,18 @@ def main() -> None:
     )
 
     # Step two Setup loggers
+    logger_context: LoggerCore.Logger = LoggerCore.Logger(
+        app_configuration=app_configuration
+    )
+    logger_context.debug('Logging is now available')
 
-    # # Retrive loggers
-    # globals.app_logger = Logger(globals.app_configuration)
-    # globals.app_logger.debug('Finished initializing loggers')
+    # Listing app configuration
+    logger_context.info('Listing app configuration')
+    for key, value in app_configuration.__dict__.items():
+        logger_context.info(f'\t{key}: {value}')
+    logger_context.info('End of configuration')
 
-    # # Listing configuration
-    # globals.app_logger.info('Listing Configuration:')
-    # for key, value in globals.app_configuration.__dict__.items():
-    #     globals.app_logger.info(f'\t{key}: {value}')
-    # globals.app_logger.info('End of configuration')
+
     # # Load discord token
     # TokenLoader.loadDiscordToken()
     # # Init Runtime and attach it to globals
