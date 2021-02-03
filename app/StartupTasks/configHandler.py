054ca54c02a32a30cfce39029227370d851b699b
Critteros <szymonkoziol3@gmail.com>
2021-02-03 19:04:33 +0100

    Completed refractoring of configHandler.py

--- app/StartupTasks/configHandler.py
+++ app/StartupTasks/configHandler.py
@@ -1,10 +1,6 @@
 # Note: This file excecutes it's code before Logging systems are loader thus logging is not possible in this file
 
 
-# App includes
-from app.Logging.LoggingGlobals import levels, console_types, logging_types
-
-# //NEW
 #########################################################################################
 # Library includes
 
@@ -14,9 +10,11 @@ from typing import List
 #########################################################################################
 # App includes
 
-
 # Types
 from app.Types import configClass as configClass
+
+# Predefined Logging Constants used to validate config files
+from app.Logging.LoggingGlobals import levels, console_types, logging_types
 #########################################################################################
 
 
@@ -51,70 +49,83 @@ def getConfiguration(*,
             config_path=config_path
         )
 
-    # Compile the configuration file
-    user_configuration: Config = loadConfig(config_path)
-    default_configuration: Config = loadConfig(default_config_path)
+    # Read the configuration files
+    user_configuration: configClass.Config = loadConfig(file_path=config_path)
+    default_configuration: Config = loadConfig(file_path=default_config_path)
 
-    Config.defaultConfig = default_configuration.__dict__
+    # Bind default configuration to configClass.py
+    configClass.Config.defaultConfig = default_configuration.__dict__
 
-    for _property in Config.properties:
-        if (hasattr(user_configuration, _property)):
-            setattr(configuration, _property,
-                    user_configuration.__dict__[_property])
+    # Take sum of two configs and combine them to one config file that will be returned
+    for value in configClass.Config.properties:
+        # Check if user specified configuration has specified property
+        if (hasattr(user_configuration, value)):
+            # If user specified has that property than append it to returned config
+            setattr(
+                app_configuration,
+                value,
+                user_configuration.__dict__[value]
+            )
+
         else:
-            setattr(configuration, _property,
-                    default_configuration.__dict__[_property])
+            # If not than append the default config's value
+            setattr(
+                app_configuration,
+                value,
+                default_configuration.__dict__[value]
+            )
 
-    _validateConfig(configuration)
-    return configuration
+    # Mutatuve function that validates the app configuration
+    _validateConfig(app_configuration)
+    return app_configuration
 
 
-def _validateConfig(config: Config) -> None:
+def _validateConfig(config: configClass.Config) -> None:
     """
     Validates config file and changes values in the config if they are out of defined range to default values. For internal use only
 
     Args:
-        config (Config): The config file to validate
+        config (configClass.Config): The config file to validate
     """
     # Defaults
-    defaults: dict = Config.defaultConfig
+    defaults: dict = configClass.Config.defaultConfig
 
-    # Log to Console
+    # Log to Console property
     specified_obj = config.log_to_console
     if(not isinstance(specified_obj, bool)):
         config.log_to_console = defaults['log_to_console']
 
-    # Log to file
+    # Log to file property
     specified_obj = config.log_to_file
     if(not isinstance(specified_obj, bool)):
         config.log_to_file = defaults['log_to_file']
 
-    # Log library
+    # Log library property
     specified_obj = config.log_library
     if(not isinstance(specified_obj, bool)):
         config.log_library = defaults['log_library']
 
-    # Console Log level
+    # Console Log level property
     specified_level = config.console_log_level
     if(specified_level not in levels):
         config.console_log_level = defaults['console_log_level']
 
-    # File Log Level
+    # File Log Level property
     specified_level = config.file_log_level
     if(specified_level not in levels):
         config.file_log_level = defaults['file_log_level']
 
-    # Library Log Level
+    # Library Log Level property
     specified_level = config.library_log_level
     if(specified_level not in levels):
         config.library_log_level = defaults['library_log_level']
 
-    # Console logger type
+    # Console logger type property
     specified_type = config.console_logger_type
     if(specified_type not in console_types):
         config.console_logger_type = defaults['console_logger_type']
 
-    # Library logging type
+    # Library logging type property
     specified_type = config.library_logging_type
     if(specified_type not in logging_types):
         config.library_logging_type = defaults['library_logging_type']
@@ -130,52 +141,34 @@ def loadConfig(*,
         file_path (pathlib.Path): Path object pointing to config file in JSON format
 
     Returns:
-        Config: Instance of Config class containing loaded config
+        configClass.Config: Instance of Config class containing loaded config
     """
 
     # Ckeck if file exists
     if(not file_path.exists()):
         raise(FileNotFoundError)
 
-    config_return: Config = Config()
+    # Config instance to be returned
+    config_return: configClass.Config = configClass.Config()
 
     # Load JSON file to a string
-    raw_json: str = loadJSONtext(file)
+    with open(file_path, 'rt') as f:
+        raw_json: str = f.read()
 
     # Try to Parse JSON file
     try:
         json_object: dict = json.loads(raw_json)
     except json.JSONDecodeError:
-        raise RuntimeError(f'Invalid JSON: {file}')
+        raise RuntimeError(f'Invalid JSON: {file_path}')
 
     # Read and save all properties that intrest us
-    for value in Config.properties:
+    for value in configClass.Config.properties:
         try:
             setattr(config_return, value, json_object[value])
         except KeyError:
             pass
 
     return config_return
-
-
-def loadJSONtext(file: Path) -> str:
-    """
-    Loads JSON file to a string 
-
-    Args:
-        file (Path): Path object pointing to a JSON file
-
-    Returns:
-        str: A string representation of a file
-    """
-    # Checks if file exist
-    if(not file.exists()):
-        raise(FileNotFoundError)
-
-    with open(file, 'rt') as f:
-        string_json: str = f.read()
-
-    return string_json
 
 
 def _createConfiguration(*,
