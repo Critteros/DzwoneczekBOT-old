50454cb42d08bab77d8b15d560b9f75594e6db6c
Critteros <szymonkoziol3@gmail.com>
2021-02-04 20:55:58 +0100

    Compleated data model

--- app/DataModel.py
+++ app/DataModel.py
@@ -8,6 +8,7 @@ import json
 import pathlib
 import os
 from datetime import datetime
+from typing import Any
 #########################################################################################
 # App includes
 
@@ -23,6 +24,7 @@ class Data:
 
         # Lock that will be used in async functions
         self.lock = asyncio.Lock()
+        self.loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
         # Check if dir exist
         try:
@@ -41,6 +43,7 @@ class Data:
 
     #########################################################################################
 
+    # Main fuction to dump data to file
     def _dump(self) -> None:
         now_time = datetime.now().strftime(date_format)
         tmp_file_name: str = f'{now_time}.tmp.json'
@@ -51,11 +54,50 @@ class Data:
 
         os.replace(tmp_file_path, path_to_data)
 
+    # Main function to load data from file
+    def _load(self) -> None:
+        try:
+            with open(path_to_data, 'rt') as f:
+                self._data = json.load(f)
+        except FileNotFoundError:
+            self._data: dict = {}
+
+    def get(self, key: Any, *args) -> Any:
+        """
+        Gets specified key from data 
+
+        Args:
+            key (Any): key to be accesed
+
+        Returns:
+            Any: Returns value from a given key
+        """
+        return self._data.get(str(key), *args)
+
     #########################################################################################
     # Async stuff
 
     async def save(self):
-        pass
+        """
+        Saves data to a file
+        """
+        async with self.lock:
+            await self.loop.run_in_executor(None, self._dump)
 
+    async def load(self):
+        """
+        Loads data form a file
+        """
+        async with self.lock:
+            await self.loop.run_in_executor(None, self._load)
 
-Data()._dump()
+    async def put(self, key: Any, value: Any):
+        """
+        Edits entry at specified key
+
+        Args:
+            key (Any): specified key
+            value (Any): value to be set
+        """
+        self._data[str(key)] = value
+        await self.save()
